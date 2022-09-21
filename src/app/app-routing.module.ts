f8632769348efd7d5abc4a3cd5f233fec2b58deb
Alexandre <alexandre.saner@gmail.com>
2022-09-21 16:14:00 +0200

    Update(game): Add rootes to game page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamePageComponent } from './game/game-page/game-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "game",
+  component: GamePageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
