0c287cd611d95598336b3adcbd4c2c7909ee590d
Alexandre <alexandre.saner@gmail.com>
2022-09-21 15:36:22 +0200

    Update(game): Create text generation componenent

---
+++ src/app/game/text-generation/text-generation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-generation',
+  templateUrl: './text-generation.component.html',
+  styleUrls: ['./text-generation.component.scss']
+})
+export class TextGenerationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
