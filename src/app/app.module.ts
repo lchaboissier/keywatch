c7c67b71023b4fff35a0731944f988b286630ac9
Alexandre <alexandre.saner@gmail.com>
2022-09-21 15:50:36 +0200

    Update(game): Created the game page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TextGenerationComponent } from './game/text-generation/text-generation.component';
+import { GamePageComponent } from './game/game-page/game-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextGenerationComponent
+    TextGenerationComponent,
+    GamePageComponent
   ],
   imports: [
     BrowserModule,
