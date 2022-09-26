902f4f6d368043438247e18edb20546bf122195d
Koalhack <alexandre.saner@gmail.com>
2022-09-26 22:19:44 +0200

    Update(game): created the algorithm for data process of the text-generation

--- src/app/game/text-generation/text-generation.component.ts
+++ src/app/game/text-generation/text-generation.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
 import { PromiseService } from 'src/app/shared/service/promise.service';
 
 @Component({
@@ -13,18 +14,40 @@ export class TextGenerationComponent implements OnInit {
   @Input() typedLetter = '';
 
   public loading: boolean = true;
-  public paragraph: any = "";
+  public text: any = "";
+  public words: any = "";
   public letters: any = "";
+
+  public lineLimit: number = 35;
+  public lines: any = [[]];
+
+  public wordlength: any = [];
+
   public wordNumber: number = 0;
   public letterNumber: number = 0;
   public active: number = 0;
 
+
   ngOnInit(): void {
     this.promise.getData().then(value => {
-      this.paragraph = value;
-      this.letters = this.paragraph[0].body.replaceAll(' ', '␣').split('');
-      this.wordNumber = this.paragraph[0].body.split(' ').length;
-      this.letterNumber = this.paragraph[0].body.split('').length;
+      
+      this.text = value;
+      this.letters = this.text[0].body.split('');
+      this.words = this.text[0].body.replaceAll(/\n/g, ' ').split(' ');
+      this.wordNumber = this.words.length;
+      this.wordlength = this.wordlength.map((word: string, index: number) => this.wordNumber - 1 == index ? word.length : word.length + 1);
+      this.words.map((word: string) => {
+        if ((this.lines[this.lines.length - 1].join('').length + word.length) >= this.lineLimit)
+          {
+            this.lines[this.lines.length] = [word];
+          } else {
+            this.lines[this.lines.length - 1].push(word);
+          }
+        }
+        );
+
+        console.log(this.lines);
+
       this.loading = false;
     })
   }
