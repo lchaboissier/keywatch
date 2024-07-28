0b72253d25969fb699419b0fab793460b762a67a
Luca Chaboissier <lucachaboissier@outlook.com>
2024-07-28 15:07:30 +0200

    fix display pages

--- docs/app/shared/model/score/model-score.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ModelScore } from './model-score';
-
-describe('ModelScore', () => {
-  it('should create an instance', () => {
-    expect(new ModelScore()).toBeTruthy();
-  });
-});
--- docs/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  env_url: "https://jsonplaceholder.typicode.com/comments"
-};
